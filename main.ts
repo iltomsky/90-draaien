49f7b1f964f2255c7a8c8366239a83b85b20a651
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-21 16:05:30 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-basic.forever(function () {
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 40)
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 40)
+input.onButtonPressed(Button.A, function () {
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 30)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 30)
     basic.pause(1000)
     Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
 })
